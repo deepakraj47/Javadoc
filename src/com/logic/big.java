536d7c5443756fa16a4910e2fa6187e65bffbe54
deepakraj47 <deepakrajthangavel@gmail.com>
2021-10-06 01:27:03 +0530

    success

---
+++ src/com/logic/big.java
@@ -0,0 +1,21 @@
+package com.logic;
+
+public class big {
+	public static void main(String[] args) {
+		int a=20,b=27,c=29;
+		if(a>b && a>c)
+		{ System.out.println(a);
+		}
+			
+		 else if  (b>a && b>c)
+		 {
+			 System.out.println(b);
+		}
+		 else 
+
+		 {
+		System.out.println(c);
+	}
+	}
+
+}
